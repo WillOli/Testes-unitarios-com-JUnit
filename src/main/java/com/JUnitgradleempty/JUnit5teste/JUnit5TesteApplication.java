6b3fc0dfdb7a15f958e0180d619dff823a8f4af1
WillOli <william.ika@gmail.com>
2023-09-21 09:04:56 -0300

    first commit

---
+++ src/main/java/com/JUnitgradleempty/JUnit5teste/JUnit5TesteApplication.java
@@ -0,0 +1,13 @@
+package com.JUnitgradleempty.JUnit5teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5TesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5TesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JUnitgradleempty/JUnit5teste/JUnit5TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JUnitgradleempty.JUnit5teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
